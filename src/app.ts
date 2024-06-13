3995628925d356c3b67dc0d441f777dde94f92eb
sola <gabriel.solap@gmail.com>
2024-06-13 19:00:29 -0300

    wip: add base structure to app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log('teste'); 
+//ENV
+require('dotenv').config();
+
+//ROUTES
+import router from './router';
+
+// EXTERNALS
+import express from 'express';
+import config from 'config';
+import db from '../config/db';
+
+const app = express();
+
+app.use(express.json());
+app.use('/api/', router);
+
+const port = config.get<number>('port');
+
+app.listen(port, async () => {
+
+	await db();
+
+	console.log('App rodando na porta 3000!');
+});
