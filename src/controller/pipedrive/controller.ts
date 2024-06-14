dd432d39384559deb9b45bc5185f552cb4538248
sola <gabriel.solap@gmail.com>
2024-06-14 09:13:06 -0300

    fix: add try catch to the controller

--- src/controller/pipedrive/controller.ts
+++ src/controller/pipedrive/controller.ts
@@ -6,29 +6,41 @@ import { getDealsByStatusRequest, getDealDetailsRequest, getDealProductsRequest 
 const pipedrive = { getDealsByStatusRequest, getDealDetailsRequest, getDealProductsRequest };
 
 export async function getDealsByStatus(req: Request, res: Response) {
-	const { status, limit } = req.body;
+	try {
+		const { status, limit } = req.body;
 
-	const { pipedriveDeals }: any = await pipedrive.getDealsByStatusRequest(status, limit);
+		const { pipedriveDeals }: any = await pipedrive.getDealsByStatusRequest(status, limit);
 
-	res.status(200).send(pipedriveDeals);
+		res.status(200).send(pipedriveDeals);
+	} catch (error: any) {
+		res.status(400).send(error.message);
+	}
 }
 
 export async function getDealDetails(req: Request, res: Response) {
-	const { id } = req.body;
+	try {
+		const { id } = req.body;
 
-	const { data }: any = await pipedrive.getDealDetailsRequest(id);
+		const { data }: any = await pipedrive.getDealDetailsRequest(id);
 
-	const dealDetails = data;
+		const dealDetails = data;
 
-	res.status(200).send(dealDetails);
+		res.status(200).send(dealDetails);
+	} catch (error: any) {
+		res.status(400).send(error.message);
+	}
 }
 
 export async function getDealProducts(req: Request, res: Response) {
-	const { id } = req.body;
+	try {
+		const { id } = req.body;
 
-	const { data }: any = await pipedrive.getDealProductsRequest(id);
+		const { data }: any = await pipedrive.getDealProductsRequest(id);
 
-	const dealDetails = data;
+		const dealDetails = data;
 
-	res.status(200).send(dealDetails);
+		res.status(200).send(dealDetails);
+	} catch (error: any) {
+		res.status(400).send(error.message);
+	}
 }
