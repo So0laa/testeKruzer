c98526b48e3651bc0cecceca835a35872bdccecf
sola <gabriel.solap@gmail.com>
2024-06-14 05:01:10 -0300

    feat: Add limit filter to getDealByStatun. Add getDealById and getDealProducts routes

--- src/routes/base/baseRouter.ts
+++ src/routes/base/baseRouter.ts
@@ -3,7 +3,11 @@ import { Router, Request, Response } from 'express';
 // import DealsRoutes from '../pipedrive/deals';
 //const {getDealsByStatus} = new DealsRoutes();
 
-import { getDealsByStatus } from '../../controller/pipedrive/deals';
+import { getDealsByStatus, getDealDetails, getDealProducts } from '../../controller/pipedrive/deals';
+
+// import { createOrders, getToken } from '../../controller/bling/orders';
+
+// import convertDealsToOrders from '../../middleware/convertDealsToOrders';
 
 const router = Router();
 
@@ -12,12 +16,30 @@ export default router
 	.get('/healthcheck', (req: Request, res: Response) => {
 		res.status(200).send('API Working!');
 	})
-	.get('/getDealsByStatus', async(req: Request, res: Response) => {
-		const {status} = req.body;
+	.get('/getDealsByStatus', async (req: Request, res: Response) => {
+		const { status, limit } = req.body;
 
-		const pipedriveDeals = await getDealsByStatus(status);
-
-		// const createOrder = await createOrdersAtBling();
+		const { pipedriveDeals }: any = await getDealsByStatus(status, limit);
 
 		res.status(200).send(pipedriveDeals);
+	})
+	.get('/getDealById', async (req: Request, res: Response) => {
+		const { id } = req.body;
+
+		const { data }: any = await getDealDetails(id);
+
+		const dealDetails = data;
+
+		res.status(200).send(dealDetails);
+	})
+	.get('/getDealProducts', async (req: Request, res: Response) => {
+		const { id } = req.body;
+
+		const { data }: any = await getDealProducts(id);
+
+		const dealDetails = data;
+
+		res.status(200).send(dealDetails);
 	});
+
+
