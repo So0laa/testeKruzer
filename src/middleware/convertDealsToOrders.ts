d40860c348abeeed2dcb00953024de5dcf55e8c7
sola <gabriel.solap@gmail.com>
2024-06-14 06:35:12 -0300

    fix: add default to the export term

--- src/middleware/convertDealsToOrders.ts
+++ src/middleware/convertDealsToOrders.ts
@@ -1,50 +1,49 @@
-export function convertDealsToOrders (dealDetails, contact, dealProductDetails) {
+export default function convertDealsToOrders (contact: number, dealDetails: any, dealProductDetails: any) {
 	const  DEFAULT_PAYMENT = 6129158;
 
-    interface order {
-    numero: number,
-    data: string,
-    dataSaida: string,
-    dataPrevista: string,
-    contato: {
-        id: number
-    }
-    itens: [{
-        quantidade: number,
-        valor: number,
-        descricao: string
-    }],
-    parcelas: [{
-        dataVencimento: string,
-        valor: number,
-        formaPagamento: {
-            id: number
-        }
-    }]
+		interface order {
+		numero: number,
+		data: string,
+		dataSaida: string,
+		dataPrevista: string,
+		contato: {
+				id: number
+		}
+		itens: [{
+				quantidade: number,
+				valor: number,
+				descricao: string
+		}],
+		parcelas: [{
+				dataVencimento: string,
+				valor: number,
+				formaPagamento: {
+						id: number
+				}
+		}]
 }
 
-    const blingOrderBody: order = {
-    	numero: dealDetails.id,
-    	data: dealDetails.won_time,
-    	dataSaida: dealDetails.update_time,
-    	dataPrevista: dealDetails.expected_close_date,
-    	contato: {
-    		id: contact
-    	},
-    	itens: [{
-    		quantidade: dealProductDetails.quantity,
-    		valor: dealProductDetails.item_price,
-    		descricao: dealProductDetails.name
-    	}],
-    	parcelas: [{
-    		dataVencimento: dealDetails.expected_close_date,
-    		valor: dealDetails.value,
-    		formaPagamento: {
-    			id: DEFAULT_PAYMENT
-    		}
-    	}]
-    };
+		const blingOrderBody: order = {
+			numero: dealDetails.id,
+			data: dealDetails.won_time,
+			dataSaida: dealDetails.update_time,
+			dataPrevista: dealDetails.expected_close_date,
+			contato: {
+				id: contact
+			},
+			itens: [{
+				quantidade: dealProductDetails.quantity,
+				valor: dealProductDetails.item_price,
+				descricao: dealProductDetails.name
+			}],
+			parcelas: [{
+				dataVencimento: dealDetails.expected_close_date,
+				valor: dealDetails.value,
+				formaPagamento: {
+					id: DEFAULT_PAYMENT
+				}
+			}]
+		};
 
-
-
+		return blingOrderBody;
 }
