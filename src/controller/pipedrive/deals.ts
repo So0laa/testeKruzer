56f8bdf841039dc68aafe152b62344a9c18fdd36
sola <gabriel.solap@gmail.com>
2024-06-14 00:37:05 -0300

    feat: add get pipedrive deal details and get pipedrive deal products functions

--- src/controller/pipedrive/deals.ts
+++ src/controller/pipedrive/deals.ts
@@ -3,29 +3,67 @@ const pipeDriveBaseUrl = process.env.PIPEDRIVE_URL;
 
 import axios from 'axios';
 
-import { Request, Response } from 'express';
+//import { Request, Response } from 'express';
 
 export async function getDealsByStatus(status: string) {
 	console.log('chegou nas deals');
 	const url: string = `${pipeDriveBaseUrl}/deals/collection?${status}`;
 
 	const config = {
-		headers: {'x-api-token': pipeDriveToken}
+		headers: { 'x-api-token': pipeDriveToken }
 	};
 
 	const dealsList = await axios.get(url, config)
 		.then((response) => {
 			return response.data;
-		}).catch(function(error){
+		}).catch(function (error) {
 			console.log('DEU RUIM');
 			console.log(error);
-		}).finally(function() {
+		}).finally(function () {
 			console.log('Chamada feita ao pipedrive');
 		});
 
 	return dealsList;
 }
 
-export async function createOrdersAtBling(req: Request, res: Response) {
-    
+export async function getDealDetails(id: number) {
+	console.log('chegou no get product');
+	const url: string = `${pipeDriveBaseUrl}/deals/${id}`;
+
+	const config = {
+		headers: { 'x-api-token': pipeDriveToken }
+	};
+
+	const dealDetails = await axios.get(url, config)
+		.then((response) => {
+			return response.data;
+		}).catch(function (error) {
+			console.log('DEU RUIM');
+			console.log(error);
+		}).finally(function () {
+			console.log('Chamada feita ao pipedrive');
+		});
+
+	return dealDetails;
+}
+
+export async function getDealProducts(id: number) {
+	console.log('chegou no get product');
+	const url: string = `${pipeDriveBaseUrl}/deals/${id}/products`;
+
+	const config = {
+		headers: { 'x-api-token': pipeDriveToken }
+	};
+
+	const productsList = await axios.get(url, config)
+		.then((response) => {
+			return response.data;
+		}).catch(function (error) {
+			console.log('DEU RUIM');
+			console.log(error);
+		}).finally(function () {
+			console.log('Chamada feita ao pipedrive');
+		});
+
+	return productsList;
 }
